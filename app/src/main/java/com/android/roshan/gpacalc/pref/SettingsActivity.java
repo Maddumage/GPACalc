c7c5436c2e428a049dad6a82ea9b7b55f309af60
Maddumage <uomroshan@gmail.com>
2017-03-20 09:54:24 +0530

    test 2017-03-20

---
+++ app/src/main/java/com/android/roshan/gpacalc/pref/SettingsActivity.java
@@ -0,0 +1,12 @@
+package com.android.roshan.gpacalc.pref;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
